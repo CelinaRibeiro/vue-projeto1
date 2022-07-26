9d908187b1dc9786b9964015dd275ab4b0e5d28e
Celina Ferreira Ribeiro <ribeiro.f.celina@gmail.com>
2022-07-26 11:52:32 -0300

    Lifecycle hooks (ciclo de vida)

---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Meu nome é {{ nome }} </h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'LifeCycle',
+        data() {
+            return {
+                nome: 'Ainda nao sei'
+            }
+        },
+        created() {
+            setTimeout(() => {
+                this.nome = 'Mateus'
+            }, 1000)
+            
+        },
+        mounted(){
+            setTimeout(() => {
+                this.nome = 'Pedro'
+            }, 2000)
+           
+        }
+    }
+</script>
