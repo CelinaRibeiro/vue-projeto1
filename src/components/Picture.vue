3a6fe2b7dca9b3c5075167cf488bae85fb889569
Celina Ferreira Ribeiro <ribeiro.f.celina@gmail.com>
2022-07-26 17:14:04 -0300

    img

---
+++ src/components/Picture.vue
@@ -0,0 +1,15 @@
+<template>
+    <img :src="bebida" :alt="descricao">
+</template>
+
+<script>
+    export default {
+        name: 'Picture',
+        data() {
+            return {
+                bebida: './img/cat1.png',
+                descricao: 'Suco'
+            }
+        }
+    }
+</script>
