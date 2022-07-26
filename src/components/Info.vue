c234c7a267819d3c6dea1a3bb7b76c4a693881c3
Celina Ferreira Ribeiro <ribeiro.f.celina@gmail.com>
2022-07-26 16:19:47 -0300

    diretivas vue

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,17 +1,26 @@
 <template>
     <div>
-        <p>Estou trabalhando no momento.</p>
+        <p v-if="trabalhando">Estou trabalhando no momento</p>
+        <p v-else>Nao estou trabalhando</p>
         <p>Uilizo as seguintes tecnologias:</p>
         <ul>
             <li>Javascript</li>
             <li>PHP</li>
             <li>Python</li>
         </ul>
+        <p v-if="estudando">Estou estudando.</p>
+        <p v-else>Não estou estudando</p>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'Info'
+        name: 'Info',
+        data() {
+            return {
+                trabalhando: false,
+                estudando: true
+            }
+        }
     }
 </script>
