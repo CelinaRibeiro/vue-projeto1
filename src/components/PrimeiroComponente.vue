6af4be85121954b9c3c448e25dda8fcd2acd2a40
Celina Ferreira Ribeiro <ribeiro.f.celina@gmail.com>
2022-07-26 11:25:43 -0300

    primeiro commit

---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Olá vue projeto1</h1>
+        <p>Meu nome é {{ nome }} e eu sou {{ profissao }} </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'PrimeiroComponente',
+        data() {
+            return {
+                nome: "José",
+                profissao: "Programador"
+            }
+        }
+    }
+</script>
