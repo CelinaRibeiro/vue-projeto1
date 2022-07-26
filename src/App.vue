608d3643e77aa6972bcdfc79b2eddd4f67ed1436
Celina Ferreira Ribeiro <ribeiro.f.celina@gmail.com>
2022-07-26 15:18:49 -0300

    components

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,22 @@
   <div>
     <PrimeiroComponente />
     <LifeCycle />
+    <Pessoa />
   </div>
 </template>
 
 <script>
   import PrimeiroComponente from './components/PrimeiroComponente.vue'
   import LifeCycle from './components/LifeCycle.vue'
+  import Pessoa from './components/Pessoa.vue'
 
   export default {
     name: 'App',
     components: {
-      PrimeiroComponente,
-      LifeCycle
-    }
+    PrimeiroComponente,
+    LifeCycle,
+    Pessoa,
+    Pessoa
+}
   }
 </script>
